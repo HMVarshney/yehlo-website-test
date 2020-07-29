ba8f2b02eb9b8690b49493636b5452d8f4e59633
HMVarshney <himank.varshney9@gmail.com>
2020-07-29 17:05:00 +0530

    error_resolve

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
 
 //Components
 import Navbar from './components/navbar';
-import Listing from './components/listings';
+import Listing from  './components/Listings/listings';
 import About from './components/about';
 import Footer from './components/Footer';
 import ProductDetails from './components/productDetails.js';
 import Home from './components/Home.js';
 import AppIcon from './components/AppIcon';
 import GetApp from './components/GetTheApp';
+import AdsListings from './components/Listings/adsListings';
 
 //css
 import styles from "./css/home.module.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Switch>
           <Route exact path='/' component={Home} />
@@ -24,13 +25,14 @@ function App() {
           <Route exact path='/about' component={About} />
           <Route exact path='/productdetails/:category/:product_id' component={ProductDetails} />
           <Route exact path='/getApp' component={GetApp} />
+          <Route exact path='/sponsored' component={AdsListings} />
           <Redirect to='/' />
       </Switch>
       <footer className={styles.footer}>
         <Footer/>
       </footer>
-      <a href='/getApp'><AppIcon /></a>
-    </BrowserRouter>
+      <Link to='/getApp'><AppIcon /></Link>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MainContextProvider from './context/context-provider/mainContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <MainContextProvider>
         <App />
     </MainContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
